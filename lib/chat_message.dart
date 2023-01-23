22c6bbe4e3368ca2f69adac71fe5372aa057917b
denzilnjagi <njagidenzil@gmail.com>
2023-01-23 19:29:07 +0300

    chat ai

---
+++ lib/chat_message.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChatMessage extends StatelessWidget {
+  const ChatMessage({Key? key, required this.text, required this.sender}) : super(key: key);
+
+  final String text;
+  final String sender;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+
+    );
+  }
+}
